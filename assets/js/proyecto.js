dddd9bec9c3e514e284ac3336e67338ad3af4303
conois <conii.echeverria@gmail.com>
2018-02-22 00:58:07 -0300

    scroll beauty

--- assets/js/proyecto.js
+++ assets/js/proyecto.js
@@ -1,5 +1,6 @@
 $(document).ready( ()=> {
-	main(); 
+	main();
+	beautyScroll(); 
 })
 
 function main(){
@@ -19,4 +20,15 @@ function main(){
 			$('body').removeClass('noscroll');
 		}
 	})
+}
+
+function beautyScroll() {
+	$('nav a').click(function(e){				
+		e.preventDefault();		//evitar el eventos del enlace normal
+		var strAncla=$(this).attr('href'); //id del ancla
+			$('body,html').stop(true,true).animate({				
+				scrollTop: $(strAncla).offset().top
+			},1000);
+		
+	});
 }
